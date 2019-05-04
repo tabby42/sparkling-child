58be5d977bd83e08135932adb0ad1b3945ff7fa9
Irene Theiss <irene-rudel@gmx.at>
2019-05-04 21:58:06 +0200

    remove unnecessary comments

--- settings.js
+++ settings.js
@@ -6,9 +6,3 @@
 
 exports.themeLocation = './';
 exports.urlToPreview = 'http://heartofthesong.local/';
-
-// Simply remove the two slashes at the front of those lines
-// to uncomment them and then delete lines #1 and #2.
-
-// Be SURE to update urlToPreview to YOUR domain and not mine.
-// Be SURE to update themeLocation to YOUR theme folder name
